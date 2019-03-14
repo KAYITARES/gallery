3f5d88072c3b0664cae1505b40cc30497a4c4b3c
MBOYIRE <mboyire14@gmail.com>
2019-03-14 11:44:39 +0200

    adding save test of LocationTest in test.py

--- photos/tests.py
+++ photos/tests.py
@@ -4,6 +4,19 @@ from .models import Location,Category,Image
 class LocationTestClass(TestCase):
     def setUp(self):
         self.Rwanda= Location(photo_location="Rwanda")
+        self.Rwanda.save_location()
        #Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.Rwanda,Location)) 
+    # Testing Save Method
+    def test_save_method(self):
+        self.Rwanda.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
+    # def test_updating_location(self):
+    #     location = Location.get_location_id(self.Rwanda.id)
+    #     location.update_location('Kitengela')
+    #     location = Location.get_location_id(self.Rwanda.id)
+    #     self.assertTrue(location.photo_location == 'Kitengela')
+    # def tearDown(self):
+    #     self.Rwanda.delete_location()
