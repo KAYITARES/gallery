c0c08f0b5dc4df018be22deeafe5820f18e3d482
MBOYIRE <mboyire14@gmail.com>
2019-03-14 12:00:51 +0200

    comment welcome to my gallery web in welcome.html

--- galleryz/settings.py
+++ galleryz/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'galleryz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tribune',
+        'NAME': 'galleryz',
         'USER': 'wecode',
     'PASSWORD':'cycy1234',
     }
