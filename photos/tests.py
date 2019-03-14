5d32f59997595fb35fde1f62a2021985e81fc33f
MBOYIRE <mboyire14@gmail.com>
2019-03-14 11:45:31 +0200

    adding update test of LocationTest in test.py

--- photos/tests.py
+++ photos/tests.py
@@ -13,10 +13,10 @@ class LocationTestClass(TestCase):
         self.Rwanda.save_location()
         location = Location.objects.all()
         self.assertTrue(len(location) > 0)
-    # def test_updating_location(self):
-    #     location = Location.get_location_id(self.Rwanda.id)
-    #     location.update_location('Kitengela')
-    #     location = Location.get_location_id(self.Rwanda.id)
-    #     self.assertTrue(location.photo_location == 'Kitengela')
+    def test_updating_location(self):
+        location = Location.get_location_id(self.Rwanda.id)
+        location.update_location('Kitengela')
+        location = Location.get_location_id(self.Rwanda.id)
+        self.assertTrue(location.photo_location == 'Kitengela')
     # def tearDown(self):
     #     self.Rwanda.delete_location()
