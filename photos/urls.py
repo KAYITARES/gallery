49e58492fb9c61211303db34d47394ad02471bec
MBOYIRE <mboyire14@gmail.com>
2019-03-14 17:23:56 +0200

    adding search url in url.py

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     
-    url('^$',views.index,name ='image')
+    url('^$',views.index,name ='home')
+    url(r'^search/', views.search, name='search_images')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
