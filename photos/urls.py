76032a4293309a3b803351d26255648eff37b4ea
MBOYIRE <mboyire14@gmail.com>
2019-03-15 15:35:06 +0200

    adding single-image link on url.py

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns=[
     
     url('^$',views.index,name ='home'),
-    url(r'^search/', views.search, name='search_images')
+    url(r'^search/', views.search, name='search_images'),
+     url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image, name='art'),
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
